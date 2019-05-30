293bcfa34a36a778808e95ae8b4bd3341b6390f5
toshiba6012 <51103416+toshiba6012@users.noreply.github.com>
2019-05-30 02:10:21 -0700

    Create video-device.c

---
+++ arch/x86/boot/video-device.c
@@ -0,0 +1,3 @@
+I dont know c but I can kind of get it. This is to support touchscreens and laptops with HDMI. This file will be called by 
+boot.h and will search HDMI-1-A and HDMI-2-A for a device. If one is found the kernel will disable LVDS-1 and eDP-1-A and 
+video=HDMI-1-A. This will take linux and Android to a new level. Thanks again!
